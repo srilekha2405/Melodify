0a813e78f6233c9c732b4b3b856e60858532eac4
srilekha2452 <srilekha3890@gmail.com>
2025-04-15 19:28:28 +0530

    change in vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-          tailwindcss()
-  ],
+          tailwindcss()],
+          // eslint-disable-next-line no-undef
+          base : process.env.VITE_BASE_PATH||"/melodify"
+
 })
