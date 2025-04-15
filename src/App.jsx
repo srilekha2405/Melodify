d62895a17a6c3b178d6f91d00a1753db72d178c0
srilekha2452 <srilekha3890@gmail.com>
2025-04-15 15:19:48 +0530

    Melodify app

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import SideBar from './components/sideBar'
+import Player from './components/Player'
+import Display from './components/Display'
+import { PlayerContext } from './context/PlayContext'
+
+
+const App = () => {
+  const {audioRef,track} =useContext(PlayerContext)
+  return (
+    <div className='h-screen bg-black'> 
+        <div className='h-[90%] flex'>
+          <SideBar/>
+          <Display/>
+        </div>
+        <Player/>
+        <audio ref={audioRef} src={track.file} preload='auto'></audio>
+      
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/SongItem.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { PlayerContext } from '../context/PlayContext'
+
+const SongItem = ({name,image,desc,id}) => {
+  const {PlayWithId}=useContext(PlayerContext)
+  return (
+    <div onClick={()=>PlayWithId(id)} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+        <img className='rounded' src={image} alt="" />
+        <p className='font-bold mt-2 mb-1'>{name}</p>
+        <p className='text-slate-200 text-sm'>{desc}</p>
+      
+    </div>
+  )
+}
+
+export default SongItem
